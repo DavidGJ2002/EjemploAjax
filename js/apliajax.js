8be48130dd63089435070e2f61955ca2e62cebba
DavidGJ2002 <dgrandej2000@alumno.ipn.mx>
2022-12-08 11:56:38 -0600

    Update apliajax.js

--- js/apliajax.js
+++ js/apliajax.js
@@ -1,17 +1,18 @@
-const boton = document.querySelector("#botonenv");
+const boton = document.querySelector('#botonenv');
 
 function loadDoc() {
   var xhttp = new XMLHttpRequest();
 
+  xhttp.open('GET', 'archivo.txt', true);
+  xhttp.onreadystatechange = function() {
+    console.log(xhttp.readyState);
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      var mostrar = document.getElementById('demo');
+      mostrar.innerHTML = xhttp.responseText;
+    }
+  }
 
-    xhttp.onreadystatechange = function() {
-       console.log(xhttp.readyState);
-      if (xhttp.readyState == 4 && xhttp.status == 200) {
-        document.getElementById("demo").innerHTML = xhttp.responseText;
-      }
-    };
-    xhttp.open("GET", "archivo.txt", true);
     xhttp.send();
 }
 
-boton.addEventListener("click", loadDoc);
+boton.addEventListener('click', loadDoc);
